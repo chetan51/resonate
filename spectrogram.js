4438dd5362c4f05dd886a403509f529bbfe37306
Chetan Surpur <chetan51@gmail.com>
2015-08-22 23:47:52 -0700

    Allow multiple of the same note in spectrogram

--- spectrogram.js
+++ spectrogram.js
@@ -17,17 +17,20 @@ window.Spectrogram = function(mathbox) {
 Spectrogram.prototype.add = function(note) {
     var self = this;
 
-    this.notes[note.keyNumber] = note;
+    var queue = this.notes[note.keyNumber] = this.notes[note.keyNumber] || [];
+    queue.push(note);
+
+    var curveId = 'note-' + note.keyNumber + '-' + queue.length;
 
     self.mathbox.curve({
-      id: 'note-' + note.keyNumber,
+      id: curveId,
       domain: self.mathbox.viewport().axis(0),
       n: 1024,
       lineWidth: 1,
       color: parseInt(randomColor({hue: 'blue'}).replace(/^#/, ''), 16),
     });
 
-    self.mathbox.animate('#note-' + note.keyNumber, {
+    self.mathbox.animate('#' + curveId, {
       expression: function (x) { return Math.sin(note.frequency() * x * 2*Math.PI); },
     }, {
       duration: self.animationDuration,
@@ -39,19 +42,23 @@ Spectrogram.prototype.add = function(note) {
 Spectrogram.prototype.remove = function(note) {
     var self = this;
 
-    delete self.notes[note.keyNumber];
+    var queue = this.notes[note.keyNumber];
+    var curveId = 'note-' + note.keyNumber + '-' + queue.length;
 
-    self.mathbox.animate('#note-' + note.keyNumber, {
+    queue.shift();
+    if (queue.length == 0) delete this.notes[note.keyNumber];
+
+    self.mathbox.animate('#' + curveId, {
       expression: function (x) { return 0; },
     }, {
       duration: self.animationDuration,
       callback: function() {
-        self.mathbox.animate('#note-' + note.keyNumber, {
+        self.mathbox.animate('#' + curveId, {
             opacity: 0,
         }, {
             duration: self.animationDuration,
             callback: function () {
-                self.mathbox.remove('#note-' + note.keyNumber);
+                self.mathbox.remove('#' + curveId);
             }
         });
       }
@@ -62,15 +69,18 @@ Spectrogram.prototype.remove = function(note) {
 
 Spectrogram.prototype.updateComposite = function() {
     var self = this;
-    var notes = _.clone(self.notes);
+    var notes = _.map(this.notes, _.clone);
 
     this.mathbox.animate('#composite', {
       expression: function (x) {
         var y = 0;
 
         for (var keyNumber in notes) {
-            var note = notes[keyNumber];
-            y += Math.sin(note.frequency() * x * 2*Math.PI);
+            var queue = notes[keyNumber];
+
+            for (var note of queue) {
+                y += Math.sin(note.frequency() * x * 2*Math.PI);
+            }
         }
 
         return y;
