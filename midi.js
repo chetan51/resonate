ed910e979627cdb99e94bb9dbcdb963b3e350b46
tomsilver <tomssilver@gmail.com>
2015-08-23 15:40:31 -0400

    first draft of midi file control

---
+++ midi.js
@@ -0,0 +1,88 @@
+window.MidiControl = function MidiControl(spectrogram) {
+
+	this.spectrogram = spectrogram;
+
+	var __audioSynth = new AudioSynth();
+	__audioSynth.setVolume(0.5);
+	var __octave = 4;
+
+	var keysPressed = [];
+	var player = MIDI.Player;
+
+	var midiFileDir = "midifiles/";
+	var midiFiles = ["test1.mid", "test2.mid"];
+	var currentMidiFile = "";
+
+	var fnVisualizeNote = function(keyNumber, keyChannel) {
+        var note = new Note(keyNumber, 127);
+
+        spectrogram.add(note);
+        setTimeout(function() {
+            spectrogram.remove(note);
+        }, 500);
+	};
+
+	var fnControlSong = function (e) {
+
+		var numKnownMidiFiles = midiFiles.length;
+
+		if (e.keyCode - 49 < numKnownMidiFiles) {
+			midiFile = midiFiles[e.keyCode-49];
+
+			if (midiFile === currentMidiFile) {
+				if (player.playing) {
+					player.pause();
+				}
+				else {
+					player.resume();
+				}
+			}
+
+			else {
+				player.stop();
+				fnPlaySongFromMidi(midiFileDir+midiFile);
+			}
+
+			currentMidiFile = midiFile;
+		}
+	}
+
+	var fnPlaySongFromMidi = function (midiFile) {
+		/// this sets up the MIDI.Player and gets things going...
+		player.removeListener();
+		player.loadFile(midiFile, player.start);
+		player.addListener(function(data) {
+			var pianoKey = data.note - 21;
+			// TODO: THIS IS A NOTE WE KNOW HOW TO PLAY
+			if (true) {
+				// note is starting to play
+				if (data.message === 144) {
+					keysPressed.push(pianoKey);
+					fnVisualizeNote(pianoKey, data.channel);
+				} 
+				// note was playing but is no longer
+				else {
+					var i = keysPressed.length;
+					while(i--) {
+						if(keysPressed[i]==pianoKey) {
+							keysPressed.splice(i, 1);
+						}
+					}
+				}
+			}
+		});
+	}
+
+	window.onload = function () {
+		MIDI.loadPlugin({
+			soundfontUrl: "./soundfont/",
+			instrument: "acoustic_grand_piano",
+			onprogress: function(state, progress) {
+				console.log(state, progress);
+			},
+			onsuccess: function() {
+				window.addEventListener('keydown', fnControlSong);
+			}
+		});
+	};
+};
